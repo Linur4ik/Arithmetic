9367ee892b577d141875fd12b0c7fb07ddae00a2
Linur4ik <83649872+Linur4ik@users.noreply.github.com>
2021-06-08 19:51:45 +0700

    Update Encoded.cpp

--- Encoded.cpp
+++ Encoded.cpp
@@ -2,38 +2,21 @@
 
 int main()
 {
-    ifstream In("Encoded.txt", ios::binary);
+    ifstream In("Text.txt", ios::binary);
     if (!In.is_open())
     {
         cout << "error";
         return 0;
     }
-    ofstream Out("Decoded.txt", ios::binary);
+    ofstream Out("Encoded.txt", ios::binary);
     if (!In.is_open())
     {
         cout << "error";
     }
-    ofstream c("Encoded.txt", ios::binary);
-    if (!In.is_open())
-    {
-        cout << "error";
-        return 0;
-    }
-    ifstream d("Text.txt", ios::binary);
-    if (!In.is_open())
-    {
-        cout << "error";
-    }
-    Arithmetic a(10);
-    Arithmetic b;
-    b.CreateMap(d);
-    b.WriteCap(c);
-    b.Encoded(d, c);
-   // b.PrintMap();
-
-
-    a.ReadCap(In);
-    a.Decoded(In, Out);
-    a.PrintMap();
-    return 0;
+    Arithmetic a(3);
+    a.CreateMap(In);
+    a.WriteCap(Out);
+    a.Encoded(In,Out);
+    //a.PrintMap();
+	return 0;
 }
